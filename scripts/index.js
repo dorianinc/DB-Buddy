aa2fbc916b74a7b9ce61cce0060ef841f93bda31
Dorian Macias <dorianrdba@gmail.com>
2024-09-23 08:39:25 -0700

    changed how service data is retrieved

--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const startApplication = async () => {
   const statusContainer = document.querySelector(".status-container");
+
+  const table = document.getElementById("servicesTable");
+  table.style.display = "none";
+
   const retryButton = document.createElement("button");
   retryButton.className = "btn btn-primary";
   retryButton.innerText = "Retry";
-  retryButton.style.display = "none"; // Start hidden
+  retryButton.style.display = "none"; 
 
   retryButton.addEventListener("click", async () => {
     retryButton.disabled = true;
@@ -16,46 +20,41 @@ const startApplication = async () => {
   });
 
   const serviceData = await fetchServiceData(statusContainer);
+  const database = serviceData.payload.database;
+  const apps = serviceData.payload.apps;
+  console.log("🖥️  serviceData: ", serviceData);
 
-  if (!serviceData) {
+  if (serviceData) {
+    retryButton.style.display = "none"; // Hide button if fetch is successful
+    statusContainer.style.display = "none";
+    table.style.display = "table";
+    populateTable(table, database, apps);
+  } else {
     retryButton.style.display = "block"; // Show button if fetch fails
     statusContainer.append(retryButton);
-  } else {
-    retryButton.style.display = "none"; // Hide button if fetch is successful
   }
 };
 
-async function fetchServiceData(statusContainer, retryButton) {
+async function fetchServiceData(statusContainer) {
   statusContainer.innerHTML = `
     <div class="spinner-border text-light" style="width: 3rem; height: 3rem" role="status"></div>
     <h2 class="text-light">Loading Web Services...</h2>
   `;
 
-  const table = document.getElementById("servicesTable");
-  table.style.display = "none";
-
   try {
     const fetchServices = await window.api.getServices();
-
+    
     // Handle both successful fetch and API-level failure
     if (!fetchServices.success) {
       throw new Error("Failed to retrieve Render Data");
     }
-
-    const database = fetchServices.payload.database;
-    const apps = fetchServices.payload.apps;
-
-    populateTable(table, database, apps);
-    statusContainer.style.display = "none";
-    table.style.display = "table";
-    return true;
-
+    return fetchServices;
   } catch (error) {
     console.error("Error fetching services:", error);
     statusContainer.innerHTML = `
       <i class="fa-solid fa-circle-exclamation fa-2xl text-danger"></i>
       <h2 class="text-danger">Failed to retrieve Render Data.</h2>
     `;
-    return false;
+    return null;
   }
 }
